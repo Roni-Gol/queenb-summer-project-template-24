c8b39cd242d32bb0c24593d7f5943f227b39850e
RoniGol <ronigol2000@gmail.com>
2024-09-29 16:58:55 +0300

    signup and login forms

--- client/src/App.js
+++ client/src/App.js
@@ -4,15 +4,15 @@ import Home from './pages/HomePage/HomePage';
 import SearchPage from './pages/SearchPage/SearchPage';
 import styles from './styles/App.module.css';
 
-<<<<<<< HEAD
+import signup from './pages/signup';
+import login from './pages/login';
+
 import ItemView from './components/ItemView';
 import LibraryView from './components/LibraryView';
-=======
 // pages & components
 import Upload from './pages/UploadPage/UploadPage';
 import Navbar from './components/Navbar';
 
->>>>>>> 49df72d2446741dc2e6ec08f1c523ae9ad7e07d0
 
 function App() {
   return (
@@ -24,6 +24,8 @@ function App() {
             <Route path="/" element={<Home/>} />
             <Route path="/upload" element={<Upload />} />
             <Route path="/SearchPage" element={<SearchPage />} />
+            <Route path="/signup" element={<signup />} />
+            <Route path="/login" element={<login />} />
           </Routes>
         </main>
         <footer className={styles.footer}>
